7453c257fd25819b1f21be74a32ebaa1550fc1c7
unknown <wowminjini@naver.com>
2018-05-29 00:57:37 +0900

    homework4

--- src/homework4/AddressBookManager.java
+++ src/homework4/AddressBookManager.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 import java.util.Map;
 import java.util.HashMap;
@@ -28,82 +29,79 @@ public class AddressBookManager {
         books.put(name,addname);
     }
 
-<<<<<<< HEAD
     public AddressBook remove(String name) {
-=======
-  public AddressBook remove(String name) {
->>>>>>> 73dc340469a7dfb2af5f73f1d507fb4e11bd0324
-        AddressBook book = null;
-        book = books.get(name);
-        books.remove(name);
-        return book;
+                AddressBook book=null;
+                book=books.get(name);
+                books.remove(name);
+                return book;
+
     }
 
 
 
-    public void printAll() {
-        for(String abc : books.keySet()) {
-            System.out.println(books.get(abc).toString());
+        public void printAll() {
+            for(String abc : books.keySet()) {
+                System.out.println(books.get(abc).toString());
+            }
         }
-    }
 
-    public AddressBook findByName (String name) {
-        AddressBook book = null;
-        if (books.containsKey(name)) {
-            book=books.get(name);
-            return book;
+        public AddressBook findByName (String name) {
+            AddressBook book = null;
+            if (books.containsKey(name)) {
+                book=books.get(name);
+                return book;
+            }
+            return null;
+
         }
-        return null;
 
-    }
+        public static void showMenu() {
+            System.out.println("===== Menu =====");
+            System.out.println("1. Add Address");
+            System.out.println("2. PrintAll");
+            System.out.println("3. Find By Name");
+            System.out.println("4. Remove By Name");
+            System.out.println("5. Quit");
+        }
 
-    public static void showMenu() {
-        System.out.println("===== Menu =====");
-        System.out.println("1. Add Address");
-        System.out.println("2. PrintAll");
-        System.out.println("3. Find By Name");
-        System.out.println("4. Remove By Name");
-        System.out.println("5. Quit");
-    }
+        public static int getMenu(Scanner sc) {
+            return Integer.parseInt(sc.nextLine().trim());
+        }
 
-    public static int getMenu(Scanner sc) {
-        return Integer.parseInt(sc.nextLine().trim());
-    }
+        public static void main(String...args) {
+            Scanner sc = new Scanner(System.in);
+            int count = 0;
 
-    public static void main(String...args) {
-        Scanner sc = new Scanner(System.in);
-        int count = 0;
+            AddressBookManager bookManager = new AddressBookManager();
 
-        AddressBookManager bookManager = new AddressBookManager();
-
-        while(true) {
-            showMenu();
-            int menu = getMenu(sc);
-            if (menu == 1) {
-                String name = sc.nextLine();
-                String phone = sc.nextLine();
-                bookManager.add(name, phone);
-            } else if (menu == 2) {
-                bookManager.printAll();
-            } else if (menu == 3) {
-                String name = sc.nextLine();
-                AddressBook book = bookManager.findByName(name);
-                if (book != null) {
-                    System.out.println("Find: " + book.toString());
-                } else {
-                    System.out.println("There is no address : " + name);
+            while(true) {
+                showMenu();
+                int menu = getMenu(sc);
+                if (menu == 1) {
+                    String name = sc.nextLine();
+                    String phone = sc.nextLine();
+                    bookManager.add(name, phone);
+                } else if (menu == 2) {
+                    bookManager.printAll();
+                } else if (menu == 3) {
+                    String name = sc.nextLine();
+                    AddressBook book = bookManager.findByName(name);
+                    if (book != null) {
+                        System.out.println("Find: " + book.toString());
+                    } else {
+                        System.out.println("There is no address : " + name);
+                    }
+                } else if (menu == 4) {
+                    String name = sc.nextLine();
+                    AddressBook book = bookManager.remove(name);
+                    if (book != null) {
+                        System.out.println("Delete: " + book.toString());
+                    } else {
+                        System.out.println("There is no address : " + name);
+                    }
+                } else if (menu == 5) {
+                    break;
                 }
-            } else if (menu == 4) {
-                String name = sc.nextLine();
-                AddressBook book = bookManager.remove(name);
-                if (book != null) {
-                    System.out.println("Delete: " + book.toString());
-                } else {
-                    System.out.println("There is no address : " + name);
-                }
-            } else if (menu == 5) {
-                break;
             }
         }
     }
-}
