dfead1d399fa99ac8ba905170827f7fd113c02d2
unknown <wowminjini@naver.com>
2018-06-12 00:43:31 +0900

    homework5

--- src/homework5/AddressBookManager.java
+++ src/homework5/AddressBookManager.java
@@ -1,9 +1,9 @@
+import java.io.*;
 import java.util.Scanner;
 import java.util.Map;
 import java.util.HashMap;
-import java.io.*;
 
-class AddressBook {
+class AddressBook{
     private String name;
     private String phone;
 
@@ -11,19 +11,32 @@ class AddressBook {
         this.name = name;
         this.phone = phone;
     }
+
     public String toString() {
         return String.format("%s(%s)", name, phone);
     }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
 }
-public class AddressBookManager {
+
+public class AddressBookManager{
     private Map<String, AddressBook> books = null;
+
     public AddressBookManager() {
         books = new HashMap<String, AddressBook>();
     }
+
     public void add(String name, String phone) {
         AddressBook book = new AddressBook(name, phone);
         books.put(name, book);
     }
+
     public AddressBook remove(String name) {
         AddressBook book = findByName(name);
         if (book != null) {
@@ -31,51 +44,57 @@ public class AddressBookManager {
         }
         return book;
     }
+
     public void printAll() {
         for (String key : books.keySet()) {
             AddressBook book = books.get(key);
             System.out.println(book.toString());
         }
     }
+
     public AddressBook findByName(String name) {
+        //Implement to find book By Name
         AddressBook book = null;
         if (books.containsKey(name)) {
             book = books.get(name);
         }
         return book;
     }
-    public boolean save(String path) {
-        if (path.equals("user.txt") == true) {
-            try {
-                FileWriter writer = new FileWriter(path);
-                for (String key : books.keySet()) {
-                    AddressBook book = books.get(key);
-                    writer.write(book.toString() + "\r\n");
-                }
-                writer.close();
-                return true;
-            } catch (IOException e) {
-                e.printStackTrace();
-                return true;
+
+    public boolean save(String path) { //Implement save method
+
+        try {
+            FileWriter writer = new FileWriter(path);
+            for(String key : books.keySet()){
+                AddressBook book = books.get(key);
+                String line = book.getName() +" " + book.getPhone();
+                writer.write(line + "\r\n");
             }
+            writer.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return false;
     }
 
     public boolean load(String path) {
-        if (path.equals("user.txt") == true) {
-            try {
-                FileWriter writer = new FileWriter(path);
-                for (String key : books.keySet()) {
-                    AddressBook book = books.get(key);
-                    writer.write(key + "\r\n");
+        try{
+            FileReader fileReader = new FileReader(path);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = null;
+            while((line = reader.readLine()) !=null){
+                String parts[] = line.split(" ");
+                if(parts.length==2){
+                    AddressBook book = new AddressBook(parts[0], parts[1]);
+                    books.put(parts[0], book);
                 }
-                writer.close();
-                return true;
-            } catch (IOException e) {
-                e.printStackTrace();
-                return true;
             }
+            reader.close();
+            fileReader.close();
+            return true;
+        } catch(IOException e){
+            e.printStackTrace();
         }
         return false;
     }
